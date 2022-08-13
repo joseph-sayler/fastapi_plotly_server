ba1650f428776ebcc6a4b07ff2d999476bee2626
Joseph Sayler <joseph.sayler@gmail.com>
2022-08-13 15:09:55 -0700

    update to fetch data from api
    added call to the api and to render the index.html template and display the data there

--- app/server/main.py
+++ app/server/main.py
@@ -1,12 +1,14 @@
-from flask import Flask
 from asgiref.wsgi import WsgiToAsgi
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+asgi_app = WsgiToAsgi(app)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-asgi_app = WsgiToAsgi(app)
+    url = "http://localhost:8000/"
+    response = requests.get(url)
+    message = response.json().get("message")
+    return render_template("index.html", api_call=message)
